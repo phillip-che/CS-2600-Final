cf488d837420cf0f1ad3381e71f9bc2f47f54fb8
Phillip Che <pche@cpp.edu>
2021-12-01 23:57:30 -0800

    Added lsh_cd command function

--- Shell/main.c
+++ Shell/main.c
@@ -149,3 +149,52 @@ int lsh_launch(char **args)
 
    return 1;
 }
+
+/*
+   Function Declarations for builtin shell commands:
+*/
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+/*
+   List of builtin commands, followed by their corresponding functions.
+*/
+char *builtin_str[] = 
+{
+   "cd",
+   "help",
+   "exit"
+};
+
+int (*builtin_func[]) (char **) == 
+{
+   &lsh_cd,
+   &lsh_help,
+   &lsh_exit
+};
+
+int lsh_num_builtins()
+{
+   return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+   Builtin function implementations.
+*/
+
+int lsh_cd(char **args)
+{
+   if (args[1] == NULL)
+   {
+      fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+   }
+   else
+   {
+      if (chdir(args[1]) != 0)
+      {
+         perror("lsh");
+      }
+   }
+   return 1;
+}
