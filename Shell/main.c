3d7c67613d092e39cb20edbf0dfceef6b05dca8f
Phillip Che <pche@cpp.edu>
2021-12-02 01:00:24 -0800

    Fixed lsh_read_line function

--- Shell/main.c
+++ Shell/main.c
@@ -5,6 +5,76 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+   Function Declarations for builtin shell commands:
+*/
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+
+/*
+   List of builtin commands, followed by their corresponding functions.
+*/
+char *builtin_str[] = 
+{
+   "cd",
+   "help",
+   "exit"
+};
+
+int (*builtin_func[]) (char **) = 
+{
+   &lsh_cd,
+   &lsh_help,
+   &lsh_exit
+};
+
+int lsh_num_builtins()
+{
+   return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+   Builtin function implementations.
+*/
+
+int lsh_cd(char **args)
+{
+   if (args[1] == NULL)
+   {
+      fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+   }
+   else
+   {
+      if (chdir(args[1]) != 0)
+      {
+         perror("lsh");
+      }
+   }
+   return 1;
+}
+
+int lsh_help(char **args)
+{
+   int i;
+   printf("Phillip Che's LSH\n");
+   printf("Type program names and arguments, and hit enter.\n");
+   printf("The following are built in:\n");
+
+   for(i = 0; i < lsh_num_builtins(); i++)
+   {
+      printf(" %s\n", builtin_str[i]);
+   }
+
+   printf("Use the man command for information on other programs.\n");
+   return 1;
+}
+
+int lsh_exit(char **args)
+{
+   return 0;
+}
+
 #define LSH_RL_BUFSIZE 1024
 char *lsh_read_line(void)
 {
@@ -25,7 +95,11 @@ char *lsh_read_line(void)
       c = getchar();
 
       // If we hit EOF, replace it with a null character and return.
-      if(c = EOF || c == '\n')
+      if(c == EOF)
+      {
+         exit(EXIT_SUCCESS);
+      }
+      else if (c == '\n')
       {
          buffer[position] = '\0';
          return buffer;
@@ -120,75 +194,7 @@ int lsh_launch(char **args)
    return 1;
 }
 
-/*
-   Function Declarations for builtin shell commands:
-*/
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
 
-/*
-   List of builtin commands, followed by their corresponding functions.
-*/
-char *builtin_str[] = 
-{
-   "cd",
-   "help",
-   "exit"
-};
-
-int (*builtin_func[]) (char **) = 
-{
-   &lsh_cd,
-   &lsh_help,
-   &lsh_exit
-};
-
-int lsh_num_builtins()
-{
-   return sizeof(builtin_str) / sizeof(char *);
-}
-
-/*
-   Builtin function implementations.
-*/
-
-int lsh_cd(char **args)
-{
-   if (args[1] == NULL)
-   {
-      fprintf(stderr, "lsh: expected argument to \"cd\"\n");
-   }
-   else
-   {
-      if (chdir(args[1]) != 0)
-      {
-         perror("lsh");
-      }
-   }
-   return 1;
-}
-
-int lsh_help(char **args)
-{
-   int i;
-   printf("Stephen Brennan's LSH\n");
-   printf("Type program names and arguments, and hit enter.\n");
-   printf("The following are built in:\n");
-
-   for(i = 0; i < lsh_num_builtins(); i++)
-   {
-      printf(" %s\n", builtin_str[i]);
-   }
-
-   printf("Use the man command for information on other programs.\n");
-   return 1;
-}
-
-int lsh_exit(char **args)
-{
-   return 0;
-}
 
 int lsh_execute(char **args)
 {
