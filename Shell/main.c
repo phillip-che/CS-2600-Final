dbb952db0466302554b7cfee87559fc375d4ccfb
Phillip Che <pche@cpp.edu>
2021-12-02 00:05:33 -0800

    Added lsh_execute function

--- Shell/main.c
+++ Shell/main.c
@@ -219,3 +219,24 @@ int lsh_exit(char **args)
 {
    return 0;
 }
+
+int lsh_execute(char **args)
+{
+   int i;
+
+   if (args[0] == NULL)
+   {
+      // An empty command was entered.
+      return 1;
+   }
+
+   for (i = 0; i < lsh_num_builtins(); i++)
+   {
+      if(strcmp(args0, builtin_str[i]) == 0)
+      {
+         return (*builtin_func[i])(args);
+      }
+   }
+
+   return lsh_launch(args);
+}
