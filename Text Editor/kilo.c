9ee9c9678873e92bd1cc679a02306b02cf0e86cb
Phillip Che <pche@cpp.edu>
2021-12-03 16:54:00 -0800

    Added tildes and cursor repositioning

--- Text Editor/kilo.c
+++ Text Editor/kilo.c
@@ -20,6 +20,9 @@ struct termios orig_termios;
 
 void die(const char *s)
 {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -63,9 +66,22 @@ char editorReadKey()
 
 /*** output ***/
 
+void editorDrawRows()
+{
+    int y;
+    for (y = 0; y < 24; y++)
+    {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
 void editorRefreshScreen()
 {
-    write(STDOUT_FILENO, "x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+
     write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
@@ -78,6 +94,8 @@ void editorProcessKeypress()
     switch (c)
     {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }
