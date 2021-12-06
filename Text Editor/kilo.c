a567eb4a7103e1857d3b700d031a3c721d913b2a
Phillip Che <pche@cpp.edu>
2021-12-06 14:42:48 -0800

    Added editorSyntaxToColor function

--- Text Editor/kilo.c
+++ Text Editor/kilo.c
@@ -263,6 +263,16 @@ void editorUpdateSyntax(erow *row)
     }
 }
 
+int editorSyntaxToColor(int hl)
+{
+    switch (hl)
+    {
+        case HL_NUMBER: 
+            return 31;
+        default: 
+            return 37;
+    }
+}
 
 /*** row operations ***/
 
